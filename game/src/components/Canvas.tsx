7e23dfd203be5a2388e770e3cafd9c3608f75222
Mikkel Silvasen <mikkelkp360@gmail.com>
2025-05-15 17:35:44 +0200

    Initial combat commit, has gameloop, missing player dying, wave clear, and loot rolling

--- game/src/components/Canvas.tsx
+++ game/src/components/Canvas.tsx
@@ -1,10 +1,4 @@
 import React from "react";
-import {
-  createWeapon,
-  createRing,
-  createAmulet,
-  createArmor,
-} from "../game/factories/EquipmentFactory";
 
 import { createDefaultCharacter } from "../game/factories/CharacterFactory";
 import { createEnemyWave } from "../game/factories/EnemyFactory";
@@ -18,75 +12,32 @@ import { goblinPrefab, orcPrefab, slimePrefab } from "../game/prefabs/prefabs";
 // Import new components
 import CharacterBox from "./CharacterBox";
 import CombatBox from "./CombatBox";
-import LootBox from "./LootBox";
+// import LootBox from "./LootBox";
 
-const wave1 = createEnemyWave([orcPrefab, orcPrefab, orcPrefab, goblinPrefab, slimePrefab]);
+const wave1 = createEnemyWave([
+  orcPrefab,
+  orcPrefab,
+  orcPrefab,
+  goblinPrefab,
+  slimePrefab,
+]);
 const allEnemyWaves = new AllEnemyWaves();
 allEnemyWaves.addWave(wave1);
 
 const enemyWaveManager = new EnemyWaveManager(allEnemyWaves);
 
 // Example inventory for demonstration (replace with your real inventory logic)
-const exampleInventory = {
-  weapon: createWeapon("sword", 1, "iron"),
-  helmet: createArmor("helmet", 1, "diamond"),
-  ring1: createRing("wood", 1),
-  ring2: createRing("iron", 1),
-  amulet: createAmulet("wood"),
-};
 
 const spritePath = "/assets/sprites";
 
 const Canvas: React.FC = () => {
   const [character, setCharacter] = React.useState(createDefaultCharacter());
-  const effectiveStats = calculateEffectiveStats(character);
+  const [effectiveStats, setEffectiveStats] = React.useState(
+    calculateEffectiveStats(character)
+  );
 
   // Get the current enemy from the wave manager
-  const currentEnemy = enemyWaveManager.currentEnemy;
   const currentWave = enemyWaveManager.currentWave;
-  const enemyIndex =
-    currentWave && currentEnemy
-      ? currentWave.enemies.findIndex((e) => e === currentEnemy)
-      : 0;
-  const enemyCount = currentWave ? currentWave.enemies.length : 0;
-
-  // Equip functions now equip from inventory (example)
-  const equipWeapon = () => {
-    setCharacter((prev) => ({
-      ...prev,
-      equipment: {
-        ...prev.equipment,
-        weapon: exampleInventory.weapon,
-      },
-    }));
-  };
-  const equipRing1 = () => {
-    setCharacter((prev) => ({
-      ...prev,
-      equipment: {
-        ...prev.equipment,
-        ring1: exampleInventory.ring1,
-      },
-    }));
-  };
-  const equipAmulet = () => {
-    setCharacter((prev) => ({
-      ...prev,
-      equipment: {
-        ...prev.equipment,
-        amulet: exampleInventory.amulet,
-      },
-    }));
-  };
-  const equipHelmet = () => {
-    setCharacter((prev) => ({
-      ...prev,
-      equipment: {
-        ...prev.equipment,
-        helmet: exampleInventory.helmet,
-      },
-    }));
-  };
 
   return (
     <section className="min-h-screen flex flex-col bg-black text-white">
@@ -99,24 +50,32 @@ const Canvas: React.FC = () => {
         <section className="flex-1 flex flex-col gap-6">
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {/* Character Box */}
-            <CharacterBox
-              character={character}
-              effectiveStats={effectiveStats}
-              equipWeapon={equipWeapon}
-              equipRing1={equipRing1}
-              equipAmulet={equipAmulet}
-              equipHelmet={equipHelmet}
-              spritePath={spritePath}
-            />
+            <div className="col-span-1">
+              <CharacterBox
+                character={character}
+                setCharacter={setCharacter}
+                effectiveStats={effectiveStats}
+                setEffectiveStats={setEffectiveStats}
+                spritePath={spritePath}
+              />
+            </div>
+
             {/* Combat Box */}
-            <CombatBox
-              currentEnemy={currentEnemy}
-              spritePath={spritePath}
-              enemyIndex={enemyIndex}
-              enemyCount={enemyCount}
-            />
+            <div className="col-span-2">
+              {currentWave && effectiveStats ? (
+                <CombatBox
+                  character={character}
+                  effectiveStats={effectiveStats}
+                  currentWave={currentWave}
+                  spritePath={spritePath}
+                />
+              ) : (
+                <div>No wave or effective stats!</div>
+              )}
+            </div>
+
             {/* Loot Box */}
-            <LootBox />
+            {/* <LootBox /> */}
           </div>
         </section>
       </main>
--- game/src/types/interfaces/stats.ts
+++ game/src/types/interfaces/stats.ts
@@ -1,5 +1,8 @@
 export interface Stats {
+  health: number;
+  mana: number;
   damage: number;
+  attackSpeed: number;
   armor: number;
   strength: number;
   dexterity: number;
